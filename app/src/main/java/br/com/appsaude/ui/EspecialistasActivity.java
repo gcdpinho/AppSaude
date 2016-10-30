822e010418b139b3fa0a7f79a39fc902a1215214
gcdpinho <gcdpinho@gmail.com>
2016-10-30 02:57:04 -0200

    Especialistas begin

---
+++ app/src/main/java/br/com/appsaude/ui/EspecialistasActivity.java
@@ -0,0 +1,15 @@
+package br.com.appsaude.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.appsaude.R;
+
+public class EspecialistasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_especialistas);
+    }
+}
