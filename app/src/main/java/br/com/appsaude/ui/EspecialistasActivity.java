a7f8ccae47415d58f0f7408d52ca9cfa88133e25
gcdpinho <gcdpinho@gmail.com>
2016-10-30 03:29:29 -0200

    Especialistas 20%

--- app/src/main/java/br/com/appsaude/ui/EspecialistasActivity.java
+++ app/src/main/java/br/com/appsaude/ui/EspecialistasActivity.java
@@ -1,7 +1,10 @@
 package br.com.appsaude.ui;
 
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import br.com.appsaude.R;
 
@@ -11,5 +14,16 @@ public class EspecialistasActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_especialistas);
+        designConfigurations();
+    }
+
+    private void designConfigurations() {
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        ab.setCustomView(R.layout.actionbar_backbutton);
+        ab.setDisplayHomeAsUpEnabled(true);
+        TextView actionBar = (TextView) findViewById(R.id.actionBarId);
+        actionBar.setText("Especialistas");
+        actionBar.setTextColor(Color.parseColor("#FFFFFF"));
     }
 }
