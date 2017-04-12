c7d2c5e4624b041067ce6268b6b04d77b3357fa9
gcdpinho <gcdpinho@gmail.com>
2017-04-11 23:09:18 -0300

    callback

---
+++ app/src/main/java/br/com/appsaude/service/Callback.java
@@ -0,0 +1,13 @@
+package br.com.appsaude.service;
+
+import android.os.Handler;
+import android.os.Message;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Callback {
+
+
+
+}
